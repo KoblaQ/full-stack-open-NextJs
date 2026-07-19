279ff464a228cf902c3c4b761c8b18fde96e2ef0
Edem <edem.quashigah@student.hamk.fi>
2026-07-19 17:04:33 +0300

    notes-app setup

---
+++ notes-app/app/layout.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ notes-app/app/page.tsx
@@ -0,0 +1,29 @@
+const Home = () => {
+  console.log('hello next.js')
+  return (
+    <div>
+      <div>
+        <h2>notes app</h2>
+        An example app for{' '}
+        <a
+          href="https://courses.mooc.fi/org/uh-cs/courses/full-stack-open-nextjs"
+          target="_blank"
+        >
+          Full Stack Open Next.js
+        </a>
+      </div>
+      <div>
+        See{' '}
+        <a
+          href="https://github.com/fullstack-hy2020/nextjs-notes"
+          target="_blank"
+        >
+          https://github.com/fullstack-hy2020/nextjs-notes
+        </a>{' '}
+        for the source code
+      </div>
+    </div>
+  )
+}
+
+export default Home
