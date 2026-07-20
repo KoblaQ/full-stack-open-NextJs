8a4fcb43091e75f693f0778fb33ce1baabbad258
Edem <edem.quashigah@student.hamk.fi>
2026-07-20 20:02:15 +0300

    6. Search

--- blogs-app/app/blogs/page.tsx
+++ blogs-app/app/blogs/page.tsx
@@ -1,14 +1,29 @@
 import Link from 'next/link'
 import { getBlogs } from '../services/blogs'
 
-const Blogs = () => {
-  const blogs = getBlogs()
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>
+}) => {
+  const { filter } = await searchParams
+  const allBlogs = getBlogs()
+  const blogsToShow = filter
+    ? allBlogs.filter((blog) =>
+        blog.title.toLowerCase().includes(filter.toLowerCase()),
+      )
+    : allBlogs
 
   return (
     <div>
       <h2>Blogs</h2>
+
+      <form action="/blogs">
+        <input type="text" name="filter" defaultValue={filter ?? ''} />
+        <button type="submit">Search</button>
+      </form>
       <ul>
-        {blogs
+        {blogsToShow
           .sort((firstBlog, secondBlog) => secondBlog.likes - firstBlog.likes)
           .map((blog) => (
             <li key={blog.id}>
