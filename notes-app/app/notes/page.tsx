6b01423cde3c847a8ed3acc291e3780e8abe10a2
Edem <edem.quashigah@student.hamk.fi>
2026-07-20 16:58:41 +0300

    Showing important notes only, server component solution

--- notes-app/app/notes/page.tsx
+++ notes-app/app/notes/page.tsx
@@ -1,23 +1,41 @@
-// import Link from 'next/link'
+import Link from 'next/link'
 import { getNotes } from '../services/notes'
-import NoteList from './NoteList'
 
-const Notes = () => {
-  const notes = getNotes()
+// import NoteList from './NoteList'
+
+const Notes = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ important?: string }>
+}) => {
+  const { important } = await searchParams
+  const showImportant = important === 'true'
+  const allNotes = getNotes()
+  const notes = showImportant
+    ? allNotes.filter((note) => note.important)
+    : allNotes
+
+  // const notes = getNotes()
   return (
     <div>
       <h2>Notes</h2>
 
-      <NoteList notes={notes} />
+      <div>
+        <Link href={showImportant ? '/notes' : '/notes?important=true'}>
+          {showImportant ? 'show all' : 'show important only'}
+        </Link>
+      </div>
 
-      {/* <ul>
+      {/* <NoteList notes={notes} /> */}
+
+      <ul>
         {notes.map((note) => (
           <li key={note.id}>
             <Link href={`/notes/${note.id}`}>{note.content}</Link>{' '}
             {note.important && <strong>(important)</strong>}
           </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   )
 }
