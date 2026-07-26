8d5ac392868940272c0e3ccbe20d18fcb5b91d6f
Edem <edem.quashigah@student.hamk.fi>
2026-07-26 21:42:14 +0300

    Users notes

---
+++ notes-app/app/users/[id]/page.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+import { getNotesByUserId, getUserById } from '../../services/users'
+
+const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params
+  const user = await getUserById(Number(id))
+  const notes = await getNotesByUserId(Number(id))
+
+  if (!user) {
+    notFound()
+  }
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Username: {user.username}</p>
+      <h3>Notes</h3>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <Link href={`/notes/${note.id}`}>{note.content}</Link>
+            {note.important && <strong> (important)</strong>}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default UserPage
