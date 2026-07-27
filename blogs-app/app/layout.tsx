9b27bf69f489831b1b98e28bc8dd3aa7ef5ce71c
Edem <edem.quashigah@student.hamk.fi>
2026-07-27 15:19:39 +0300

    9. Users

--- blogs-app/app/layout.tsx
+++ blogs-app/app/layout.tsx
@@ -12,6 +12,8 @@ export default function RootLayout({
         {' | '}
         <Link href={'/blogs'}>blogs</Link>
         {' | '}
+        <Link href={'/users'}>users</Link>
+        {' | '}
         <Link href={'/blogs/new'}>Add blog</Link>
         {children}
       </body>
