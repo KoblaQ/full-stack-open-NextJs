1f7d26bd70253a9d06f26be2f2160d5eba53a5d6
Edem <edem.quashigah@student.hamk.fi>
2026-07-30 14:53:02 +0300

    13. Validations in blog creation

--- blogs-app/app/blogs/new/page.tsx
+++ blogs-app/app/blogs/new/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+
+import { useActionState } from 'react'
 import { createBlog } from '../../actions/blogs'
 
 const NewBlog = () => {
+  const [state, formAction] = useActionState(createBlog, { error: '' })
   return (
     <div>
       <h2>Create a new blog</h2>
-      <form action={createBlog}>
+      {/* <form action={createBlog}> */}
+      <form action={formAction}>
         <div>
           <label>
             Title: <input type="text" name="title" required />
